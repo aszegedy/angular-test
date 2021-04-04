8d32bdc12b3138dbb820839d318f4b75a5d8cfa4
aszegedy <andras.szegedy@gmail.com>
2021-04-04 23:30:33 +0200

    3.setTimeout button

--- src/app/warningalert/warningalert.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-warning-alert',
-  templateUrl: './warningalert.component.html',
-  styleUrls: ['./warningalert.component.css'],
-})
-export class WarningAlertComponent {}
